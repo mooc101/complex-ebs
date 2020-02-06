a1408562c700e036503f1966b0f75c9024fc7767
Sandesh Kumar <sandesh@moven.com>
2020-02-05 20:52:12 -0500

    Dockerize all components

--- client/src/App.js
+++ client/src/App.js
@@ -8,18 +8,20 @@ import NewPage from "./NewPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to Complex</h1>
-        <Link to="/">Home</Link>
-        <Link to="/newpage">New Page</Link>
-      </header>
-      <div>
-        <Route exact path="/" component={Fib} />
-        <Route path="/newpage" component={NewPage} />
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Complex</h1>
+          <Link to="/">Home</Link>
+          <Link to="/newpage">New Page</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/newpage" component={NewPage} />
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 

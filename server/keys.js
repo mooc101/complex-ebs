0a995c481691cf9a6a92cd504023f07e7c156f90
Sandesh Kumar <sandesh@moven.com>
2020-02-06 03:05:39 -0500

    Add Dockerrun and include deployment in travis.yml
    NOTE:
    - Dockerrun is added to configure ECS
    - Add deployment configuration for travis ci
    - Added some documentation around architectural choices and AWS
    configuration

--- server/keys.js
+++ server/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
   pgDb: process.env.PG_DB,
   pgHost: process.env.PG_HOST,
-  pgPass: process.env.PG_PASSWORD,
+  pgPass: process.env.PG_PASS,
   pgPort: process.env.PG_PORT,
   pgUser: process.env.PG_USER,
   redisHost: process.env.REDIS_HOST,
